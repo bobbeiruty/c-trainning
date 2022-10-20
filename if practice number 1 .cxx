7510688108c6a5344d16ffd4ff27703006be679b
bobbeiruty <112206183+bobbeiruty@users.noreply.github.com>
2022-10-20 21:44:25 +0300

    If practicing

---
+++ if practice number 1 .cxx
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int age;
+	printf("please enter your age \n");
+	scanf("%d",&age);
+	if(age>=21){
+		printf("congratulation you are eligibile for casting ");
+		
+		
+		
+	}else 
+		printf("sorry your age is not eligibile yet ");
+	
+}
